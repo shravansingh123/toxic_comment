9001b15f348c6e2e13992de382a157977fc98465
shravansingh123 <shan.icwa@gmail.com>
2021-01-11 18:28:52 +0530

    Create code.py

---
+++ code/code/code.py
@@ -0,0 +1,532 @@
+# %% [code]
+import os
+import torch
+import pandas as pd
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torch.optim import lr_scheduler
+
+from sklearn import model_selection
+from sklearn import metrics
+import transformers
+import tokenizers
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
+from tqdm.autonotebook import tqdm
+import utils
+import string
+#from tqdm import tqdm
+
+# %% [code]
+class config:
+    MAX_LEN = 128
+    TRAIN_BATCH_SIZE = 32
+    VALID_BATCH_SIZE = 8
+    EPOCHS = 5
+    BERT_PATH = "../input/roberta-base"
+    MODEL_PATH = "model.bin"
+    TRAINING_FILE = "../input/tweet-train-folds/train_folds.csv"
+    #ROBERTA_PATH = "../input/roberta-base"
+    TOKENIZER = tokenizers.ByteLevelBPETokenizer(
+    vocab_file=f"{BERT_PATH}/vocab.json", 
+    merges_file=f"{BERT_PATH}/merges.txt", 
+    lowercase=True,
+    add_prefix_space=True)
+
+DIRECTORY='../input/roberta5fold'    
+MAX_LEN = 192
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 8
+EPOCHS = 5
+ROBERTA_PATH = "../input/roberta-base"
+TOKENIZER = tokenizers.ByteLevelBPETokenizer(
+        vocab_file=f"{ROBERTA_PATH}/vocab.json", 
+    merges_file=f"{ROBERTA_PATH}/merges.txt", 
+    lowercase=True,
+    add_prefix_space=True
+)
+
+# %% [code]
+class TweetModel(transformers.BertPreTrainedModel):
+    def __init__(self, conf):
+        super(TweetModel, self).__init__(conf)
+        self.roberta = transformers.RobertaModel.from_pretrained(ROBERTA_PATH, config=conf)
+        self.drop_out = nn.Dropout(0.1)
+        self.l0 = nn.Linear(768 * 2, 2)
+        torch.nn.init.normal_(self.l0.weight, std=0.02)
+    
+    def forward(self, ids, mask, token_type_ids):
+        _, _, out = self.roberta(
+            ids,
+            attention_mask=mask,
+            token_type_ids=token_type_ids
+        )
+        print(out[-1].shape)
+        out = torch.cat((out[-1], out[-2]), dim=-1)
+        print(out.shape)
+        out = self.drop_out(out)
+        logits = self.l0(out)
+        print(logits.shape)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        print(start_logits.shape)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        return start_logits, end_logits
+
+# %% [code]
+def process_data(tweet, selected_text, sentiment, tokenizer, max_len):
+    #print(tweet)
+    tweet = " " + " ".join(str(tweet).split())
+    selected_text = " " + " ".join(str(selected_text).split())
+
+    len_st = len(selected_text) - 1
+    idx0 = None
+    idx1 = None
+
+    for ind in (i for i, e in enumerate(tweet) if e == selected_text[1]):
+        if " " + tweet[ind: ind+len_st] == selected_text:
+            idx0 = ind
+            idx1 = ind + len_st - 1
+            break
+
+    char_targets = [0] * len(tweet)
+    if idx0 != None and idx1 != None:
+        for ct in range(idx0, idx1 + 1):
+            char_targets[ct] = 1 #setting char_targets to 1 from start to end of selected text
+    
+    tok_tweet = tokenizer.encode(tweet)
+    #print(tokenizer,tok_tweet,tok_tweet.ids,tok_tweet.tokens)
+    input_ids_orig = tok_tweet.ids
+    tweet_offsets = tok_tweet.offsets
+    target_idx = []
+    for j, (offset1, offset2) in enumerate(tweet_offsets):
+        #print (offset1, offset2)
+        if sum(char_targets[offset1: offset2]) > 0:
+            target_idx.append(j)
+    
+    targets_start = target_idx[0]
+    targets_end = target_idx[-1]
+
+    sentiment_id = {
+        'positive': 1313,
+        'negative': 2430,
+        'neutral': 7974
+    }
+    
+    input_ids = [0] + [sentiment_id[sentiment]] + [2] + [2] + input_ids_orig + [2]
+    token_type_ids = [0, 0, 0, 0] + [0] * (len(input_ids_orig) + 1)
+    mask = [1] * len(token_type_ids)
+    tweet_offsets = [(0, 0)] * 4 + tweet_offsets + [(0, 0)]
+    targets_start += 4
+    targets_end += 4
+
+    padding_length = max_len - len(input_ids)
+    if padding_length > 0:
+        input_ids = input_ids + ([1] * padding_length)
+        mask = mask + ([0] * padding_length)
+        token_type_ids = token_type_ids + ([0] * padding_length)
+        tweet_offsets = tweet_offsets + ([(0, 0)] * padding_length)
+    
+    return {
+        'ids': input_ids,
+        'mask': mask,
+        'token_type_ids': token_type_ids,
+        'targets_start': targets_start,
+        'targets_end': targets_end,
+        'orig_tweet': tweet,
+        'orig_selected': selected_text,
+        'sentiment': sentiment,
+        'offsets': tweet_offsets
+    }
+
+
+class TweetDataset:
+    def __init__(self, tweet, sentiment, selected_text):
+        self.tweet = tweet
+        self.sentiment = sentiment
+        self.selected_text = selected_text
+        self.tokenizer = TOKENIZER
+        self.max_len = MAX_LEN
+    
+    def __len__(self):
+        return len(self.tweet)
+
+    def __getitem__(self, item):
+        data = process_data(
+            self.tweet[item], 
+            self.selected_text[item], 
+            self.sentiment[item],
+            self.tokenizer,
+            self.max_len
+        )
+
+        return {
+            'ids': torch.tensor(data["ids"], dtype=torch.long),
+            'mask': torch.tensor(data["mask"], dtype=torch.long),
+            'token_type_ids': torch.tensor(data["token_type_ids"], dtype=torch.long),
+            'targets_start': torch.tensor(data["targets_start"], dtype=torch.long),
+            'targets_end': torch.tensor(data["targets_end"], dtype=torch.long),
+            'orig_tweet': data["orig_tweet"],
+            'orig_selected': data["orig_selected"],
+            'sentiment': data["sentiment"],
+            'offsets': torch.tensor(data["offsets"], dtype=torch.long)
+        }
+
+# %% [code]
+def calculate_jaccard_score(
+    original_tweet, 
+    target_string, 
+    sentiment_val, 
+    idx_start, 
+    idx_end, 
+    offsets,
+    verbose=False):
+    
+    if idx_end < idx_start:
+        idx_end = idx_start
+    
+    filtered_output  = ""
+    for ix in range(idx_start, idx_end + 1):
+        filtered_output += original_tweet[offsets[ix][0]: offsets[ix][1]]
+        if (ix+1) < len(offsets) and offsets[ix][1] < offsets[ix+1][0]:
+            filtered_output += " "
+
+    if sentiment_val == "neutral" or len(original_tweet.split()) < 2:
+        filtered_output = original_tweet
+
+    if sentiment_val != "neutral" and verbose == True:
+        if filtered_output.strip().lower() != target_string.strip().lower():
+            print("********************************")
+            print(f"Output= {filtered_output.strip()}")
+            print(f"Target= {target_string.strip()}")
+            print(f"Tweet= {original_tweet.strip()}")
+            print("********************************")
+
+    jac =  utils.jaccard(target_string.strip(), filtered_output.strip())
+    return jac, filtered_output
+
+# %% [code]
+def loss_fn(start_logits, end_logits, start_positions, end_positions):
+    loss_fct = nn.CrossEntropyLoss()
+    start_loss = loss_fct(start_logits, start_positions)
+    end_loss = loss_fct(end_logits, end_positions)
+    total_loss = (start_loss + end_loss)
+    return total_loss
+
+# %% [code]
+def train_fn(data_loader, model, optimizer, device, scheduler=None):
+    model.train()
+    losses = utils.AverageMeter()
+    jaccards = utils.AverageMeter()
+
+    tk0 = tqdm(data_loader, total=len(data_loader))
+    
+    for bi, d in enumerate(tk0):
+
+        ids = d["ids"]
+        token_type_ids = d["token_type_ids"]
+        mask = d["mask"]
+        targets_start = d["targets_start"]
+        targets_end = d["targets_end"]
+        sentiment = d["sentiment"]
+        orig_selected = d["orig_selected"]
+        orig_tweet = d["orig_tweet"]
+        targets_start = d["targets_start"]
+        targets_end = d["targets_end"]
+        offsets = d["offsets"]
+
+        ids = ids.to(device, dtype=torch.long)
+        token_type_ids = token_type_ids.to(device, dtype=torch.long)
+        mask = mask.to(device, dtype=torch.long)
+        targets_start = targets_start.to(device, dtype=torch.long)
+        targets_end = targets_end.to(device, dtype=torch.long)
+
+        model.zero_grad()
+        outputs_start, outputs_end = model(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids,
+        )
+        loss = loss_fn(outputs_start, outputs_end, targets_start, targets_end)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        outputs_start = torch.softmax(outputs_start, dim=1).cpu().detach().numpy()
+        outputs_end = torch.softmax(outputs_end, dim=1).cpu().detach().numpy()
+        jaccard_scores = []
+        for px, tweet in enumerate(orig_tweet):
+            selected_tweet = orig_selected[px]
+            tweet_sentiment = sentiment[px]
+            jaccard_score, _ = calculate_jaccard_score(
+                original_tweet=tweet,
+                target_string=selected_tweet,
+                sentiment_val=tweet_sentiment,
+                idx_start=np.argmax(outputs_start[px, :]),
+                idx_end=np.argmax(outputs_end[px, :]),
+                offsets=offsets[px]
+            )
+            jaccard_scores.append(jaccard_score)
+
+        jaccards.update(np.mean(jaccard_scores), ids.size(0))
+        losses.update(loss.item(), ids.size(0))
+        tk0.set_postfix(loss=losses.avg, jaccard=jaccards.avg)
+
+# %% [code]
+def eval_fn(data_loader, model, device):
+    model.eval()
+    losses = utils.AverageMeter()
+    jaccards = utils.AverageMeter()
+    
+    with torch.no_grad():
+        tk0 = tqdm(data_loader, total=len(data_loader))
+        for bi, d in enumerate(tk0):
+            ids = d["ids"]
+            token_type_ids = d["token_type_ids"]
+            mask = d["mask"]
+            sentiment = d["sentiment"]
+            orig_selected = d["orig_selected"]
+            orig_tweet = d["orig_tweet"]
+            targets_start = d["targets_start"]
+            targets_end = d["targets_end"]
+            offsets = d["offsets"].numpy()
+
+            ids = ids.to(device, dtype=torch.long)
+            token_type_ids = token_type_ids.to(device, dtype=torch.long)
+            mask = mask.to(device, dtype=torch.long)
+            targets_start = targets_start.to(device, dtype=torch.long)
+            targets_end = targets_end.to(device, dtype=torch.long)
+
+            outputs_start, outputs_end = model(
+                ids=ids,
+                mask=mask,
+                token_type_ids=token_type_ids
+            )
+            loss = loss_fn(outputs_start, outputs_end, targets_start, targets_end)
+            outputs_start = torch.softmax(outputs_start, dim=1).cpu().detach().numpy()
+            outputs_end = torch.softmax(outputs_end, dim=1).cpu().detach().numpy()
+            jaccard_scores = []
+            for px, tweet in enumerate(orig_tweet):
+                selected_tweet = orig_selected[px]
+                tweet_sentiment = sentiment[px]
+                jaccard_score, _ = calculate_jaccard_score(
+                    original_tweet=tweet,
+                    target_string=selected_tweet,
+                    sentiment_val=tweet_sentiment,
+                    idx_start=np.argmax(outputs_start[px, :]),
+                    idx_end=np.argmax(outputs_end[px, :]),
+                    offsets=offsets[px]
+                )
+                jaccard_scores.append(jaccard_score)
+
+            jaccards.update(np.mean(jaccard_scores), ids.size(0))
+            losses.update(loss.item(), ids.size(0))
+            tk0.set_postfix(loss=losses.avg, jaccard=jaccards.avg)
+    
+    print(f"Jaccard = {jaccards.avg}")
+    return jaccards.avg
+
+# %% [code]
+def run(fold):
+    dfx = pd.read_csv(config.TRAINING_FILE)
+
+    df_train = dfx[dfx.kfold != fold].reset_index(drop=True)
+    df_valid = dfx[dfx.kfold == fold].reset_index(drop=True)
+    
+    train_dataset = TweetDataset(
+        tweet=df_train.text.values,
+        sentiment=df_train.sentiment.values,
+        selected_text=df_train.selected_text.values
+    )
+
+    train_data_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=config.TRAIN_BATCH_SIZE,
+        num_workers=4
+    )
+
+    valid_dataset = TweetDataset(
+        tweet=df_valid.text.values,
+        sentiment=df_valid.sentiment.values,
+        selected_text=df_valid.selected_text.values
+    )
+
+    valid_data_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=config.VALID_BATCH_SIZE,
+        num_workers=2
+    )
+
+    device = torch.device("cuda")
+    model_config = transformers.BertConfig.from_pretrained(config.BERT_PATH)
+    model_config.output_hidden_states = True
+    model = TweetModel(conf=model_config)
+    model.to(device)
+
+    num_train_steps = int(len(df_train) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
+    param_optimizer = list(model.named_parameters())
+    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    optimizer_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.001},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
+    ]
+    optimizer = AdamW(optimizer_parameters, lr=3e-5)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, 
+        num_warmup_steps=0, 
+        num_training_steps=num_train_steps
+    )
+
+    es = utils.EarlyStopping(patience=2, mode="max")
+    print(f"Training is Starting for fold={fold}")
+    
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    # I'm training only for 3 epochs even though I specified 5!!!
+    for epoch in range(5):
+        train_fn(train_data_loader, model, optimizer, device, scheduler=scheduler)
+        jaccard = eval_fn(valid_data_loader, model, device)
+        print(f"Jaccard Score = {jaccard}")
+        es(jaccard, model, model_path=f"{DIRECTORY}/model_{fold}.bin")
+        if es.early_stop:
+            print("Early stopping")
+            break
+
+# %% [code]
+#run(fold=0)
+#run(fold=1)
+#run(fold=2)
+#run(fold=3)
+#run(fold=4)
+
+# %% [code]
+df_test = pd.read_csv("../input/testingsample/testsample.csv")
+#df_test = pd.read_csv("../input/tweet-sentiment-extraction/test.csv")
+df_test.loc[:, "selected_text"] = df_test.text.values
+
+# %% [code]
+device = torch.device("cuda")
+model_config = transformers.RobertaConfig.from_pretrained(ROBERTA_PATH)
+model_config.output_hidden_states = True
+
+# %% [code]
+model1 = TweetModel(conf=model_config)
+model1.to(device)
+model1.load_state_dict(torch.load(f"{DIRECTORY}/model_0.bin"))
+model1.eval()
+
+model2 = TweetModel(conf=model_config)
+model2.to(device)
+model2.load_state_dict(torch.load(f"{DIRECTORY}/model_1.bin"))
+model2.eval()
+
+model3 = TweetModel(conf=model_config)
+model3.to(device)
+model3.load_state_dict(torch.load(f"{DIRECTORY}/model_2.bin"))
+model3.eval()
+
+model4 = TweetModel(conf=model_config)
+model4.to(device)
+model4.load_state_dict(torch.load(f"{DIRECTORY}/model_3.bin"))
+model4.eval()
+
+model5 = TweetModel(conf=model_config)
+model5.to(device)
+model5.load_state_dict(torch.load(f"{DIRECTORY}/model_4.bin"))
+model5.eval()
+
+
+# %% [code]
+final_output = []
+
+# %% [code]
+test_dataset = TweetDataset(
+        tweet=df_test.text.values,
+        sentiment=df_test.sentiment.values,
+        selected_text=df_test.selected_text.values
+    )
+
+data_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    shuffle=False,
+    batch_size=VALID_BATCH_SIZE,
+    num_workers=1
+)
+
+
+with torch.no_grad():
+    tk0 = tqdm(data_loader, total=len(data_loader))
+    for bi, d in enumerate(tk0):
+        ids = d["ids"]
+        token_type_ids = d["token_type_ids"]
+        mask = d["mask"]
+        sentiment = d["sentiment"]
+        orig_selected = d["orig_selected"]
+        orig_tweet = d["orig_tweet"]
+        targets_start = d["targets_start"]
+        targets_end = d["targets_end"]
+        offsets = d["offsets"].numpy()
+
+        ids = ids.to(device, dtype=torch.long)
+        token_type_ids = token_type_ids.to(device, dtype=torch.long)
+        mask = mask.to(device, dtype=torch.long)
+        targets_start = targets_start.to(device, dtype=torch.long)
+        targets_end = targets_end.to(device, dtype=torch.long)
+
+        outputs_start1, outputs_end1 = model1(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+        
+        outputs_start2, outputs_end2 = model2(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+        
+        outputs_start3, outputs_end3 = model3(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+        
+        outputs_start4, outputs_end4 = model4(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+        
+        outputs_start5, outputs_end5 = model5(
+            ids=ids,
+            mask=mask,
+            token_type_ids=token_type_ids
+        )
+        outputs_start = (outputs_start1 + outputs_start2 + outputs_start3 + outputs_start4 + outputs_start5) / 5
+        outputs_end = (outputs_end1 + outputs_end2 + outputs_end3 + outputs_end4 + outputs_end5) / 5
+        
+        outputs_start = torch.softmax(outputs_start, dim=1).cpu().detach().numpy()
+        outputs_end = torch.softmax(outputs_end, dim=1).cpu().detach().numpy()
+        jaccard_scores = []
+        for px, tweet in enumerate(orig_tweet):
+            selected_tweet = orig_selected[px]
+            tweet_sentiment = sentiment[px]
+            _, output_sentence = calculate_jaccard_score(
+                original_tweet=tweet,
+                target_string=selected_tweet,
+                sentiment_val=tweet_sentiment,
+                idx_start=np.argmax(outputs_start[px, :]),
+                idx_end=np.argmax(outputs_end[px, :]),
+                offsets=offsets[px]
+            )
+            final_output.append(output_sentence)
+
+# %% [code]
+sample = pd.read_csv("../input/tweet-sentiment-extraction/sample_submission.csv")
+sample.loc[:, 'selected_text'] = final_output
+sample.to_csv("submission.csv", index=False)
+
+# %% [code]
+sample.head()
